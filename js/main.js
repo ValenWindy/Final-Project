5dcba2dd4b21a73e45c72fc7dd29d54aa08d080a
ValenWindy <valentinrlloret@gmail.com>
2022-11-20 19:17:41 -0300

    Modificado 20/11

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+const submit = document.getElementById("submit")
+
+
+submit.addEventListener("click", clicked, e=> {
+    e.preventDefault()
+    
+})
+
+function clicked() {
+    alert("Gracias")
+}
